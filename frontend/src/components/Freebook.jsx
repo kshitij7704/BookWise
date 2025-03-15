2274e834ac38f48256c7888e317e9580abbf5365
kshitij7704 <kashyapkshitij7704@gmail.com>
2025-03-15 14:41:39 +0530

    Added login and signup

--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import list from "../../public/list.json";
-import Cards from "./Cards";
 
+import axios from "axios";
+
+import Cards from "./Cards";
 function Freebook() {
-  const data = list.filter((data) => data.category === "Free");
-  console.log(data);
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
 
   var settings = {
     dots: true,
@@ -43,10 +58,9 @@ function Freebook() {
       },
     ],
   };
-
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div>
           <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
           <p>
@@ -58,7 +72,7 @@ function Freebook() {
 
         <div>
           <Slider {...settings}>
-            {data.map((item) => (
+            {book.map((item) => (
               <Cards item={item} key={item.id} />
             ))}
           </Slider>
@@ -67,5 +81,4 @@ function Freebook() {
     </>
   );
 }
-
 export default Freebook;
